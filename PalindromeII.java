effb113b9ed82e17f80a8abd19d007c8d1960a59
tanve13 <171332553+tanve13@users.noreply.github.com>
2025-12-18 18:50:25 +0530

    Create PalindromeII.java

---
+++ PalindromeII.java
@@ -0,0 +1,39 @@
+public class PalindromeII {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.validPalindrome("aba"));     // true
+        System.out.println(solution.validPalindrome("abca"));    // true
+        System.out.println(solution.validPalindrome("abc"));     // false
+    }
+}
+
+class Solution {
+
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try skipping either left or right character
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
